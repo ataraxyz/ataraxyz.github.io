465c3c1154facf99d2e4598965543bbb7e3770e7
ataraxyz <atara.art.xyz@gmail.com>
2021-11-30 15:34:15 +1100

    changed interactive website to be available without link

---
+++ gc_interactive/face.js
@@ -0,0 +1,27 @@
+const video = document.getElementById('video')
+
+Promise.all([
+    //faceapi.nets.tinyFaceDetector.loadFromUri('http://localhost:8000/models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models')
+// localhost:8000/models
+    //faceapi.nets.tinyFaceDetector.loadFromUri('https://www.sample.com/face_recognition/final/models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    // const displaySize = { width: video.width, height: video.height }
+    // faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      if ( detections.length < 1 )
+        refresh();
+    }, 1000)
+  })
